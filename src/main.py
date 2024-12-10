adb3225440079dc21fd9392fb27c72c868519618
FrancescoSpena <francesco.spena4@gmail.com>
2024-12-10 13:00:21 +0100

    Fix label of graph

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ print(f"Using device: {device}")
 
 state_dim = 6  
 action_dim = 3  
-policy_path = "../models/Original_loss_policy_model.pth"
+policy_path = "../models/Huber_policy_model.pth"
 
 policy_model = PolicyModel(input_dim=state_dim, action_dim=action_dim).to(device)
 policy_model.load_state_dict(torch.load(policy_path, map_location=device))
@@ -50,9 +50,9 @@ def plot_loss_with_average(loss_values):
     plt.plot(epoch_numbers, loss_values, label='Loss', color='blue', alpha=0.6)
     plt.plot(epoch_numbers, cumulative_average, label='Average Loss', color='orange', linewidth=2)
     
-    plt.title('Loss and Average Loss Trend')
+    plt.title('Reward Trend')
     plt.xlabel('Epochs')
-    plt.ylabel('Loss')
+    plt.ylabel('Reward')
     plt.legend()
     plt.grid(True)
     plt.show()
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     parser.add_argument('--render', action='store_true')
     args = parser.parse_args()
 
-    num_episodes = 1
+    num_episodes = 1000
 
     if args.render:
         env = gym.make('Acrobot-v1', render_mode='human')
@@ -76,7 +76,7 @@ if __name__ == "__main__":
         rewards.append(total_reward)
         print(f"Episode {episode + 1}/{num_episodes}: Total Reward = {total_reward:.2f}")
     
-    #plot_loss_with_average(rewards)
+    plot_loss_with_average(rewards)
     
     env.close()
     print(f"Average Reward over {num_episodes} episodes: {sum(rewards) / num_episodes:.2f}")
